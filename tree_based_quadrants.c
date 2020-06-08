0edb60a4c57f5b3b2b2a7771647ab52dedc1bcf0
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2020-06-08 15:39:10 +0200

    The rest of the functions

--- tree_based_quadrants.c
+++ tree_based_quadrants.c
@@ -1,21 +1,56 @@
 #include "tree_based_quadrants.h"
 
+int
+is_valid (const quadrant_t * q)
+{
+  return
+    (q->level >= 0 && q->level <= MAXLEVEL) &&
+    ((q->x & (QUADRANT_LEN (q->level) - 1)) == 0) &&
+    ((q->y & (QUADRANT_LEN (q->level) - 1)) == 0) &&
+    ((q->z & (QUADRANT_LEN (q->level) - 1)) == 0);
+}
+
+int
+child_id (const quadrant_t * q)
+{
+  int                 res = 0;
+  res += (q->x & QUADRANT_LEN (q->level) ? 1 : 0);
+  res += ((q->y & QUADRANT_LEN (q->level) ? 1 : 0) << 1);
+  res += ((q->z & QUADRANT_LEN (q->level) ? 1 : 0) << 2);
+
+  return res;
+}
+
+void
+child (const quadrant_t * q, quadrant_t * child_quadrant, int child_id)
+{
+  child_quadrant->level = q->level + 1;
+
+  child_quadrant->x =
+    q->x + (child_id & 1 ? QUADRANT_LEN (child_quadrant->level) : 0);
+  child_quadrant->y =
+    q->y + (child_id & 2 ? QUADRANT_LEN (child_quadrant->level) : 0);
+  child_quadrant->z =
+    q->z + (child_id & 4 ? QUADRANT_LEN (child_quadrant->level) : 0);
+}
+
 void
 parent (const quadrant_t * q, quadrant_t * parent_quadrant)
 {
-    if ( q->level == 0 ) {
-        printf("The root has no parent quadrant");
-        return;
-    } 
-    // level of the parent decreases by 1
-    parent_quadrant->level = q->level - 1;
-    // we compute the size
-    int h = QUADRANT_LEN(q->level);
-    // compute parent coordinates
-    parent_quadrant->x = q->x & ~h;
-    parent_quadrant->y = q->y & ~h;
-    parent_quadrant->z = q->z & ~h;
+  if (q->level == 0) {
+    printf ("The root has no parent quadrant");
     return;
+  }
+  // level of the parent decreases by 1
+  parent_quadrant->level = q->level - 1;
+  // we compute the size
+  int                 h = QUADRANT_LEN (q->level);
+  // compute parent coordinates
+  parent_quadrant->x = q->x & ~h;
+  parent_quadrant->y = q->y & ~h;
+  parent_quadrant->z = q->z & ~h;
+
+  return;
 }
 
 void
@@ -26,17 +61,102 @@ root (quadrant_t * q)
 }
 
 void
+sibling (const quadrant_t * q, quadrant_t * sibling_quadrant, int sibling_id)
+{
+
+  if (!is_valid (q)) {
+    printf ("sibling: the input quadrant has to be valid.\n");
+    return;
+  }
+
+  if (sibling_id < 0 || sibling_id > 7) {
+    printf ("sibling: sibling_id should lie between 0 (included) \
+        and 7 (included).\n");
+    return;
+  }
+
+  sibling_quadrant->level = q->level;
+  // we compute the size
+  int                 h = QUADRANT_LEN (q->level);
+  // compute parent coordinates
+  sibling_quadrant->x = (sibling_id & 1) ? (q->x | h) : (q->x & ~h);
+  sibling_quadrant->y = (sibling_id & 2) ? (q->y | h) : (q->y & ~h);
+  sibling_quadrant->z = (sibling_id & 4) ? (q->z | h) : (q->z & ~h);
+}
+
+void
+ancestor (const quadrant_t * q, int level,quadrant_t * ancestor_quadrant)
+{
+  qcoord_t        mask;
+
+  if (!is_valid (q)) {
+    printf ("ancestor: the input quadrant has to be valid.\n");
+    return;
+  }
+
+  if((int) q->level < level || level < 0) {
+    printf("ancestor: level should be between 0 and quadrant level");
+    return;
+  }
+  mask = ~(QUADRANT_LEN(level) - 1);
+
+  ancestor_quadrant->level = level;
+  ancestor_quadrant->x = q->x & mask;
+  ancestor_quadrant->y = q->y & mask;
+  ancestor_quadrant->z = q->z & mask;
+}
+
+int
+is_ancestor (const quadrant_t * q, quadrant_t * r)
+{
+  qcoord_t          mask;
+
+  if (!is_valid (q) || !is_valid (r)) {
+    printf ("is_ancestor: the input quadrants have to be valid.\n");
+    return -1;
+  }
+
+  mask = ~(QUADRANT_LEN(q->level) - 1);
+  return (q->level <= r->level) && !((q->x ^ r->x) & mask)
+                                && !((q->y ^ r->y) & mask)
+                                && !((q->z ^ r->z) & mask);
+}
+
+void
+first_descendant (const quadrant_t * q, quadrant_t * first_descendant,
+                  int level)
+{
+
+  if (!is_valid (q)) {
+    printf ("first_descendant: the input quadrant has to be valid.\n");
+    return;
+  }
+
+  if ((int) q->level > level || level >= MAXLEVEL) {
+    printf
+      ("first_descendant: level should lie between quadrant level (included) \
+        and MAXLEVEL (excluded).\n");
+    return;
+  }
+
+  first_descendant->x = q->x;
+  first_descendant->y = q->y;
+  first_descendant->z = q->z;
+
+  first_descendant->level = (int8_t) level;
+}
+
+void
 last_descendant (const quadrant_t * q, quadrant_t * last_descendant,
                  int level)
 {
   qcoord_t            shift;
 
-#if 0
   if (!is_valid (q)) {
     printf ("last_descendant: the input quadrant has to be valid.\n");
     return;
   }
-#endif
+
   if ((int) q->level > level || level >= MAXLEVEL) {
     printf
       ("last_descendant: level should lie between quadrant level (included) \
--- tree_based_quadrants.h
+++ tree_based_quadrants.h
@@ -29,10 +29,24 @@ typedef struct quadrant
 }
 quadrant_t;
 
+/** Test if a quadrant has valid Morton indices.
+ * \param [in] q Quadrant to be tested.
+ * \return Returns true if \a q is valid.
+ */
 int                 is_valid (const quadrant_t * q);
 
+/** Compute the position of this child within its siblings.
+ * \return Returns its child id in 0..d
+ */
 int                 child_id (const quadrant_t * q);
 
+/** Compute a specific child of a quadrant.
+ * \param [in]     q    Input quadrant.
+ * \param [in,out] child_quadrant
+ *                      Existing quadrant whose Morton index will be filled
+ *                      with the coordinates of its child no. \b child_id.
+ * \param [in] child_id The id of the child computed, 0..d.
+ */
 void                child (const quadrant_t * q, quadrant_t * child_quadrant,
                            int child_id);
 
@@ -41,9 +55,7 @@ void                child (const quadrant_t * q, quadrant_t * child_quadrant,
  * \param [in,out] parent_quadrant
  *                   Existing quadrant whose Morton index will be filled
  *                   with the Morton index of the parent of \a q.
- *                   Its user_data will be untouched.
  * \note \a q may point to the same quadrant as \a parent_quadrant.
-         The user_data of \a parent_quadrant is never modified.
  */
 void                parent (const quadrant_t * q,
                             quadrant_t * parent_quadrant);
@@ -53,14 +65,40 @@ void                parent (const quadrant_t * q,
  */
 void                root (quadrant_t * q);
 
+/** Compute a specific sibling of a quadrant.
+ * \param [in]     q  Input quadrant.
+ * \param [in,out] sibling_quadrant
+ *                    Existing quadrant whose Morton index will be filled
+ *                    with the coordinates of sibling no. sibling_id of q.
+ * \param [in]     sibling_id The id of the sibling computed, 0..d.
+ */
 void                sibling (const quadrant_t * q,
                              quadrant_t * sibling_quadrant, int sibling_id);
 
+/** Compute the ancestor of a quadrant at a given level.
+ * \param [in]  q       Input quadrant.
+ * \param [in]  level   A smaller or equal level than q.
+ * \param [in,out]  ancestor_quadrant
+ *                      Existing quadrant whose Morton index will be filled
+ *                      with the ancestor of q at the given level.
+ * \note The quadrant q may point to the same quadrant as ancestor_quadrant.
+ */
 void                ancestor (const quadrant_t * q, int level,
                               quadrant_t * ancestor_quadrant);
 
-int                 is_ancestor (const quadrant_t * q, quadrant_t * r);
+/** Test if a quadrant is an ancestor of another quadrant.
+ * \param [in] q Quadrant to be tested.
+ * \param [in] r Descendent quadrant.
+ * \return true if \a q is an ancestor of \a r (might be equal).
+ */
+int                 is_ancestor (const quadrant_t * q, const quadrant_t * r);
 
+/** Compute the first descendant of a quadrant on a given level.
+ * \param [in]  q      Input quadrant.
+ * \param [out] first_descendant
+ *                     First descendant of \a q on level \a level.
+ * \param [in]  level  Level must be greater equal than q's level.
+ */
 void                first_descendant (const quadrant_t * q,
                                       quadrant_t * first_descendant,
                                       int level);
