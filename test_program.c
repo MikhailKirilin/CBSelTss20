98dc420c36abc768398e68821d5c080b914cb128
OliverPotocki <oliver.potocki@gmail.com>
2020-06-12 15:41:37 +0200

    added is_valid test for root

--- test_program.c
+++ test_program.c
@@ -3,15 +3,11 @@
 int
 main ()
 {
-  quadrant_t          q, ld;
+  quadrant_t          q;
 
   root (&q);
-  printf ("q.x = %i, q.y = %i, q.z = %i, level = %i\n", q.x, q.y, q.z,
-          q.level);
-  if (last_descendant (&q, &ld, 1) == 0)
-    /* error message will be printed by last_descendant */
-    printf ("ld.x = %i, ld.y = %i, ld.z = %i, level = %i\n", ld.x, ld.y, ld.z,
-            ld.level);
+  printf("is_valid(q) = %i\n", is_valid(&q));
+
 
   return 0;
 }
