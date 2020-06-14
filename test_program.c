20c1412ff5c55b371e05fb42099bc7ff67a32dd6
gullochini <guglielmo.chini@outlook.it>
2020-06-14 12:39:08 +0200

    test_program for child and sibling functions

--- test_program.c
+++ test_program.c
@@ -3,15 +3,37 @@
 int
 main ()
 {
-  quadrant_t          q, ld;
+  quadrant_t          r, q, sib;
+  quadrant_t          children[8];
+  //test of child and sibling functions
+  int                 flag = 1;
+  // we assign root and its first descendant at level 1
+  root (&r);
+  // we assign the children
+  for (int i = 0; i < 8; i++) {
+    child(&r, &children[i], i);
+  }
+  // we check that all the children of the root are siblings
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++) {
+      if (i == j) continue;
+      // we compute the j-th sibling of the i-th child 
+      sibling(&children[i], &sib, j);
+      // we compute the i-th sibling of the the j-th sibling of th i-th child
+       sibling(&sib, &q, i);
+      // wh check if it is the same of the i-th child of the root
+      if ( children[i].x - q.x != 0 
+        || children[i].y - q.y != 0 
+        || children[i].z - q.z != 0 ) {
+        printf("Error: the %d-th child of the root is has not the %d-th chlid of the root as %d-sibling\n",
+                i, j, j);
+        flag = 0;
+        break;
+        }
+      
+    }
 
-  root (&q);
-  printf ("q.x = %i, q.y = %i, q.z = %i, level = %i\n", q.x, q.y, q.z,
-          q.level);
-  if (last_descendant (&q, &ld, 1) == 0)
-    /* error message will be printed by last_descendant */
-    printf ("ld.x = %i, ld.y = %i, ld.z = %i, level = %i\n", ld.x, ld.y, ld.z,
-            ld.level);
-
+  if (flag)
+    printf( "All good: ad expected the i-th child of the root is has the j-th chlid of the root as j-sibling for any i and j\n");
   return 0;
 }
