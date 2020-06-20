2af66829bc83348d04b93520d9f5106f8af4c5b0
grmaier <64373365+grmaier@users.noreply.github.com>
2020-06-21 00:53:11 +0200

    test program for is_child

--- test_program.c
+++ test_program.c
@@ -14,24 +14,7 @@ main ()
             ld.level);
   
   
-	//GM
-	printf("\nTest of the function child_id:\n");
-	quadrant_t	c;
-	printf("Root quadrant q: \n");
-	root (&q);
-	printf ("q.x = %i, q.y = %i, q.z = %i, level = %i\n", q.x, q.y, q.z,
-		  q.level);
-	printf("Child quadrants c with child_ids i=0,...,8:\n");
-	// Generate child quadrants c with child_ids i=0,...,8 and print their coordinates, level and child_id
-	for (int i = 0; i <= 8; i++) {
-		child(&q, &c, i);
-		// Expect error message in last oputput line for i=8
-		if (i == 8)
-			printf("Expect error message for child_id(c) = 8:\n");
-		printf ("c.x = %i, c.y = %i, c.z = %i, level = %i, child_id(c) = %i\n", c.x, c.y, c.z,
-            c.level, child_id(&c));
-	}
-	
+	//GM	
 	printf("\nTest of the function is_child:\n");
 	quadrant_t	c1, c2;
 	
