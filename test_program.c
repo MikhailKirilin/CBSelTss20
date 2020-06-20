6fb05258dcec7e0aa1c834574685f9b06e98acc7
grmaier <64373365+grmaier@users.noreply.github.com>
2020-06-21 00:29:56 +0200

    Add test program for function is_child

--- test_program.c
+++ test_program.c
@@ -31,6 +31,35 @@ main ()
 		printf ("c.x = %i, c.y = %i, c.z = %i, level = %i, child_id(c) = %i\n", c.x, c.y, c.z,
             c.level, child_id(&c));
 	}
+	
+	printf("\nTest of the function is_child:\n");
+	quadrant_t	c1, c2;
+	
+	printf("Root quadrant q: \n");
+	root (&q);
+	printf ("q.x = %i, q.y = %i, q.z = %i, level = %i\n", q.x, q.y, q.z,
+		  q.level);
+		  
+	// Generate child quadrant c1 of q
+	printf("Child quadrant c1 of q: \n");
+	child(&q, &c1, 1);
+	printf ("c1.x = %i, c1.y = %i, c1.z = %i, level = %i, child_id(c1) = %i\n", c1.x, c1.y, c1.z,
+		  c1.level, child_id(&c1));
+	printf("c1 is child of q: %i (expect 1)\n", is_child(&c1, &q));
+	
+	// Generate child quadrants c2 of c1
+	printf("Child quadrant c2 of c1: \n");
+	child(&c1, &c2, 2);
+	printf ("c2.x = %i, c2.y = %i, c2.z = %i, level = %i, child_id(c2) = %i\n", c2.x, c2.y, c2.z,
+		  c2.level, child_id(&c2));
+		  
+	// Expect 1
+	printf("c2 is child of q: %i (expect 0)\n", is_child(&c2, &q));
+	// Expect 0
+	printf("c2 is child of c1: %i (expect 1)\n", is_child(&c2, &c1));
+	// Expect -1
+	printf("c2 is child of q: %i (expect -1)\n", is_child(&q, &c1));
+
 	return 0;
 }
 
