95a2e792cd3ca535b67a20fe2a62922d43d32a3d
grmaier <64373365+grmaier@users.noreply.github.com>
2020-07-06 03:44:01 +0200

    test smallest_ancestor()

--- examples/test_smallest_ancestor.c
+++ examples/test_smallest_ancestor.c
@@ -5,20 +5,19 @@ main ()
 {
   //TG: test last_descendant
 
-  quadrant_t          r, c1, c2, c3, c4, sm_ancestor;
-  quadrant_lin		ancestor_lin_id, dummy;
+  quadrant_t          r, c1, c2, c3, sm_ancestor;
+  quadrant_lin		sm_ancestor_lin_id, c1_lin_id;
 
   root (&r);
   child (&r, &c1, 1);
-  child (&r, &c2, 0);
-  child (&c1, &c3, 0);
-  child (&c1, &c4, 1);
+  child (&c1, &c2, 0);
+  child (&c1, &c3, 1);
   
-  smallest_ancestor(&c3, &c4, &sm_ancestor);
-  linear_id(&sm_ancestor, &ancestor_lin_id);
-  linear_id(&c1, &dummy);
-  printf("dummy_lin_id = %i\n", dummy.I);
-  printf("smallest_ancestor_lin_id = %i", ancestor_lin_id.I);
+  smallest_ancestor(&c2, &c3, &sm_ancestor);
+  linear_id(&sm_ancestor, &sm_ancestor_lin_id);
+  linear_id(&c1, &c1_lin_id);
+  printf("c1_lin_id = %i\n", c1_lin_id.I);
+  printf("smallest_ancestor_lin_id = %i", sm_ancestor_lin_id.I);
   
   return 0;
 }
